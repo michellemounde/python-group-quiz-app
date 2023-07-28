462e1da31ce43e722907b3ab57e93abdef89c552
Michelle Mounde <mitchmounde@gmail.com>
2023-07-28 11:44:38 +0300

    feat: link home template to work with home wtf form

--- app/forms/submit_form.py
+++ app/forms/submit_form.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
+from wtforms.validators import DataRequired
 
 class SubmitForm(FlaskForm):
-  search = StringField('search')
+  search = StringField('Search', validators=[DataRequired()])
